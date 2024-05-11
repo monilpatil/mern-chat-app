7bf69c7b50a552cda0e5d38379ddc6283af3af87
monil patil <monilpatil342@gmail.com>
2024-05-11 20:16:38 +0530

    last

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from 'dotenv'
+import path from "path"
 import cookieParser from "cookie-parser"
 import authRoutes from './routes/auth.js'
 import messageRoutes from './routes/message.route.js'
@@ -7,7 +8,10 @@ import userRoutes from './routes/user.route.js'
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js"
 
+
 const PORT = process.env.PORT || 3001;
+
+const __dirname = path.resolve();
 
 dotenv.config()
 
@@ -17,8 +21,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("hellow")
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 
 server.listen(PORT, () => {
